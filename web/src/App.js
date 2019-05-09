21be1d7475237a88b6523022886f6beb8ad8d611
AM_NiceDay <a.krasikau@gmail.com>
2019-05-09 17:58:55 +0300

    Reload

--- web/src/App.js
+++ web/src/App.js
@@ -4,7 +4,7 @@ import Todos from "./Todos";
 function App() {
   return (
     <div style={{ margin: "auto", width: 500 }}>
-      <h1 style={{ textAlign: "center" }}>Todo app</h1>
+      <h1 style={{ textAlign: "center" }}>Todo App</h1>
       <div style={{ border: "1px solid red", padding: 16 }}>
         <Todos />
       </div>
